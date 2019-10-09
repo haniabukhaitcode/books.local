9653fe89c03ee891a8855bf7dfdc1667406b8cdc
haniabukhaitcode <haniabukhait@gmail.com>
2019-10-09 18:47:28 +0300

    fuck life

--- public_html/view.php
+++ public_html/view.php
@@ -1,2 +1,6 @@
 https://www.codeofaninja.com/2015/06/php-crud-with-ajax-and-oop.html
 https://github.com/rodrigofmp/php-tutorial-crud-03-1-ajax/blob/master/create.php
+
+https://www.sourcecodester.com/php/11720/php-oop-crud-operation-using-ajaxjquery.html
+
+https://www.sourcecodester.com/php/12127/php-crud-operation-using-pdo-bootstrapmodal.html
